5dde6731b9515b5fceff822582c0272fb702429d
bhandekunal16 <bhandekunal16@gmail.com>
2026-09-10 10:20:04 +0530

    Refactor dependency management by consolidating service imports into a new dependency map

---
+++ depandancyMap.js
@@ -0,0 +1,52 @@
+const [faker, mock] = [
+  require("./service/fakeData"),
+  require("./service/mockData"),
+];
+
+const OpenMeteo = require("./service/OpenMeteo");
+const RestCountries = require("./service/restCountries");
+const Pokemon = require("./service/pokemon");
+const RickAndMorty = require("./service/rickAndMorty");
+const CatFacts = require("./service/catFacts");
+const DogApi = require("./service/dogApi");
+const Jikan = require("./service/jikan");
+const Coingecko = require("./service/coingecko");
+const Ipify = require("./service/ipify");
+const Agify = require("./service/agify");
+const Genderize = require("./service/genderize");
+const Nationalize = require("./service/nationalize");
+const Github = require("./service/github");
+
+const restCountries = new RestCountries();
+const fakeData = new faker();
+const mockData = new mock();
+const openMeteo = new OpenMeteo();
+const pokemon = new Pokemon();
+const rickAndMorty = new RickAndMorty();
+const catFacts = new CatFacts();
+const dogApi = new DogApi();
+const jikan = new Jikan();
+const coingecko = new Coingecko();
+const ipify = new Ipify();
+const agify = new Agify();
+const genderize = new Genderize();
+const nationalize = new Nationalize();
+const github = new Github();
+
+module.exports = {
+  github,
+  nationalize,
+  restCountries,
+  fakeData,
+  openMeteo,
+  mockData,
+  pokemon,
+  rickAndMorty,
+  catFacts,
+  dogApi,
+  jikan,
+  coingecko,
+  ipify,
+  agify,
+  genderize,
+};
--- index.js
+++ index.js
@@ -1,39 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 const { host, port } = require("./jsons/app.json");
-const faker = require("./service/fakeData");
-const mock = require("./service/mockData");
-const OpenMeteo = require("./service/OpenMeteo");
-const RestCountries = require("./service/restCountries");
-const Pokemon = require("./service/pokemon");
-const RickAndMorty = require("./service/rickAndMorty");
-const CatFacts = require("./service/catFacts");
-const DogApi = require("./service/dogApi");
-const Jikan = require("./service/jikan");
-const Coingecko = require("./service/coingecko");
-const Ipify = require("./service/ipify");
-const Agify = require("./service/agify");
-const Genderize = require("./service/genderize");
-const Nationalize = require("./service/nationalize");
-const Github = require('./service/github')
-
-const restCountries = new RestCountries();
-const fakeData = new faker();
-const mockData = new mock();
-const openMeteo = new OpenMeteo();
-const pokemon = new Pokemon();
-const rickAndMorty = new RickAndMorty();
-const catFacts = new CatFacts();
-const dogApi = new DogApi();
-const jikan = new Jikan();
-const coingecko = new Coingecko();
-const ipify = new Ipify();
-const agify = new Agify();
-const genderize = new Genderize();
-const nationalize = new Nationalize();
-const github = new Github()
+const {
+  github,
+  nationalize,
+  restCountries,
+  fakeData,
+  openMeteo,
+  mockData,
+  pokemon,
+  rickAndMorty,
+  catFacts,
+  dogApi,
+  jikan,
+  coingecko,
+  ipify,
+  agify,
+  genderize,
+} = require("./depandancyMap");
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
